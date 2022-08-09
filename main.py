5295a96a675bad2163a6f15b63f41759d0aa34db
thebowenfeng <857514.leofeng@gmail.com>
2022-08-09 16:53:08 +1000

    Deployed to heroku

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import aiohttp
 from io import BytesIO
 import base64
 import requests
+from PIL import Image
 
 with open("token.txt", "r") as file:
     TOKEN = file.read()
@@ -20,16 +21,33 @@ async def on_ready():
 
 @bot.slash_command(description="Draw some images with a prompt")
 async def draw(ctx, prompt: discord.Option(str)):
+    print(f"LOG: {ctx.author} requested a draw with prompt {prompt}")
+
     response = await ctx.send(f"<@{ctx.author.id}> Drawing image with prompt: {prompt}. This could take up to 1 minute...")
     async with aiohttp.ClientSession() as sess:
         async with sess.post("https://backend.craiyon.com/generate",
                          headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"},
                          json={"prompt": prompt}) as resp:
             images = await resp.json()
-            image1 = images["images"][0]
 
-    image = discord.File(BytesIO(base64.b64decode(image1)))
-    image.filename = "test.png"
+    print(f"LOG: Image data for {ctx.author} and prompt {prompt} received")
+
+    pil_images = [Image.open(BytesIO(base64.b64decode(image))) for image in images["images"]]
+    merged = Image.new(mode="RGB", size=(pil_images[0].width * 3, pil_images[0].height * 3))
+
+    index = 0
+    for col in range(0, 3):
+        for row in range(0, 3):
+            merged.paste(pil_images[index], (col * pil_images[0].width, row * pil_images[0].height))
+            index += 1
+
+    print(f"LOG: Image data for {ctx.author} and prompt {prompt} merged")
+
+    img_bytes = BytesIO()
+    merged.save(img_bytes, format="PNG")
+    img_bytes.seek(0)
+    image = discord.File(img_bytes)
+    image.filename = "result.png"
     await response.edit(content=f"<@{ctx.author.id}> Your \"{prompt}\" images has been generated", file=image)
 
 
